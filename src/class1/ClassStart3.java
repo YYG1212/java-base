b860684b33b4faa446d6956818147a835f42e747
yang-yungi <joee1212@gmail.com>
2023-12-16 00:09:41 +0900

    자바 기본 공부 클래스

--- src/class1/ClassStart3.java
+++ src/class1/ClassStart3.java
@@ -4,7 +4,7 @@ public class ClassStart3 {
     public static void main(String[] args) {
         Student student1;
         student1 = new Student();
-        student1.name = "학생111";
+        student1.name = "학생1";
         student1.age = 15;
         student1.grade = 90;
 
