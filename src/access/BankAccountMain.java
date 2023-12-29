8683b56bd246318da93cc299de3110b957668b19
yang-yungi <joee1212@gmail.com>
2023-12-29 15:36:41 +0900

    접근제어자 캡슐화

---
+++ src/access/BankAccountMain.java
@@ -0,0 +1,10 @@
+package access;
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.deposit(3000);
+        account.withdraw(4000);
+        System.out.println("balance = " + account.getBalance());
+    }
+}
